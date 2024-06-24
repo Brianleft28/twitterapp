d62d4a7d5e27957802f309af7257b336535b6b28
BrianLEFT <contactobrianleft@gmail.com>
2024-06-24 13:11:34 -0300

    next app

--- client/src/components/home/index.jsx
+++
@@ -1,4 +0,0 @@
-const Home = () => {
-  return <div>asd</div>;
-};
-export default Home;
--- client/src/layouts/pageLayout.jsx
+++
@@ -1,9 +0,0 @@
-const PageLayout = ({ children }) => {
-  return (
-    <>
-      <div className="bg-red-300 p-3">{children}</div>
-    </>
-  );
-};
-
-export default PageLayout;
