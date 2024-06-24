ee14513c78be2d364731470ff0d15e899b5d800f
BrianLEFT <contactobrianleft@gmail.com>
2024-06-24 12:58:07 -0300

    commit structure

---
+++ client/src/components/home/index.jsx
@@ -0,0 +1,4 @@
+const Home = () => {
+  return <div>asd</div>;
+};
+export default Home;
---
+++ client/src/layouts/pageLayout.jsx
@@ -0,0 +1,9 @@
+const PageLayout = ({ children }) => {
+  return (
+    <>
+      <div className="bg-red-300 p-3">{children}</div>
+    </>
+  );
+};
+
+export default PageLayout;
